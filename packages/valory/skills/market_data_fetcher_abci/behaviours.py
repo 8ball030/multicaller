77b209bdc41351f2da0347c15dd508dd5f872f85
David Vilela <dvilelaf@gmail.com>
2024-01-11 15:59:50 +0100

    fix: use address as key

--- packages/valory/skills/market_data_fetcher_abci/behaviours.py
+++ packages/valory/skills/market_data_fetcher_abci/behaviours.py
@@ -142,11 +142,8 @@ class FetchMarketDataBehaviour(MarketDataFetcherBaseBehaviour):
         }
 
         # Get the market data for each token
-        import pdb
-
-        pdb.set_trace()
         for token_data in self.params.token_symbol_whitelist:
-            token_id = token_data.get("coingecko", None)
+            token_id = token_data.get("coingecko_id", None)
 
             if not token_id:
                 self.context.logger.error(
@@ -167,8 +164,7 @@ class FetchMarketDataBehaviour(MarketDataFetcherBaseBehaviour):
 
             self.context.logger.info(f"Succesfully fecthed market for {token_id}")
 
-            # We use Jupiter symbol as the market key
-            markets[token_data["jupiter"]] = response_json["prices"]
+            markets[token_data["address"]] = response_json["prices"]
 
         # Send to IPFS
         data_hash = None
