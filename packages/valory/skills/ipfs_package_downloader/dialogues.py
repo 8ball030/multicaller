6614273874a5c0ca003b6c850f6f56dc80c00133
David Vilela <dvilelaf@gmail.com>
2024-01-11 10:14:47 +0100

    fix: linters

--- packages/valory/skills/ipfs_package_downloader/dialogues.py
+++ packages/valory/skills/ipfs_package_downloader/dialogues.py
@@ -25,17 +25,10 @@ from aea.protocols.base import Message
 from aea.protocols.dialogue.base import Dialogue as BaseDialogue
 from aea.skills.base import Model
 
-from packages.valory.protocols.default.dialogues import (
-    DefaultDialogue as BaseDefaultDialogue,
-)
-from packages.valory.protocols.default.dialogues import (
-    DefaultDialogues as BaseDefaultDialogues,
-)
 from packages.valory.protocols.ipfs.dialogues import IpfsDialogue as BaseIpfsDialogue
 from packages.valory.protocols.ipfs.dialogues import IpfsDialogues as BaseIpfsDialogues
 
 
-DefaultDialogue = BaseDefaultDialogue
 IpfsDialogue = BaseIpfsDialogue
 
 
@@ -64,31 +57,5 @@ class IpfsDialogues(Model, BaseIpfsDialogues):
         BaseIpfsDialogues.__init__(
             self,
             self_address=str(self.skill_id),
-            role_from_first_message=role_from_first_message,
-        )
-
-
-class DefaultDialogues(Model, BaseDefaultDialogues):
-    """The dialogues class keeps track of all dialogues."""
-
-    def __init__(self, **kwargs: Any) -> None:
-        """Initialize dialogues."""
-        Model.__init__(self, **kwargs)
-
-        def role_from_first_message(
-            message: Message, receiver_address: Address
-        ) -> BaseDialogue.Role:
-            """Infer the role of the agent from an incoming/outgoing first message
-
-            :param message: an incoming/outgoing first message
-            :param receiver_address: the address of the receiving agent
-            :return: The role of the agent
-            """
-            self.context.logger.debug(f"{message} {receiver_address}")
-            return DefaultDialogue.Role.AGENT
-
-        BaseDefaultDialogues.__init__(
-            self,
-            self_address=self.context.agent_address,
             role_from_first_message=role_from_first_message,
         )
