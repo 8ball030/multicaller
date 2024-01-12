7dc1853d19b31f2ac23a8895dff88fa97a89424f
angrybayblade <vptl185@gmail.com>
2024-01-12 16:37:52 +0530

    feat: update server to request tx from jupiter

--- proxy/server.js
+++ proxy/server.js
@@ -1,33 +1,60 @@
+const fs = require('fs');
 const express = require('express');
-const { Connection, Transaction, clusterApiUrl, SystemProgram, PublicKey } = require('@solana/web3.js');
+const bs58 = require('bs58');
+const fetch = require('cross-fetch');
+const { Wallet } = require('@project-serum/anchor');
+const { Connection, Keypair, Transaction, clusterApiUrl } = require('@solana/web3.js');
 
 const rpc = clusterApiUrl('devnet');
-const key = fs.readFileSync('./solana_private_key.txt', 'utf-8')
+const key = fs.readFileSync('./solana_private_key.txt', 'utf-8').trim()
+const keypair = Keypair.fromSecretKey(bs58.decode(key));
+const wallet = new Wallet(keypair);
 
 const port = 3000;
 const app = express();
 
 app.post('/tx', async (req, res) => {
-    const { instructions, recent_blockhash } = req.body;
+    const { inputMint, outputMint, amount, slippageBps } = req.body
     const connection = new Connection(rpc, 'confirmed');
-    const walletKeyPair = new Transaction().add({
-        keys: [{ pubkey: new PublicKey(key), isSigner: true, isWritable: true }],
-        programId: SystemProgram.programId,
-        data: Buffer.alloc(0),
+
+    const quoteResponse = await (
+        await fetch(`https://quote-api.jup.ag/v6/quote
+            ?inputMint=${inputMint}\
+            &outputMint=${outputMint}\
+            &amount=${amount}\
+            &slippageBps=${slippageBps}`
+        )
+    ).json();
+
+    const { swapTransaction } = await (
+        await fetch('https://quote-api.jup.ag/v6/swap', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                quoteResponse,
+                userPublicKey: wallet.publicKey.toString(),
+                wrapAndUnwrapSol: true,
+            })
+        })
+    ).json();
+
+    const swapTransactionBuf = Buffer.from(swapTransaction, 'base64');
+    var transaction = VersionedTransaction.deserialize(swapTransactionBuf);
+    transaction.sign([wallet.payer]);
+
+    // Execute the transaction
+    const rawTransaction = transaction.serialize()
+    const txid = await connection.sendRawTransaction(rawTransaction, {
+        skipPreflight: true,
+        maxRetries: 2
     });
-
-    const transaction = new Transaction().add(instructions);
-    transaction.recentBlockhash = recent_blockhash;
-    transaction.sign(walletKeyPair);
-
-    const signature = await connection.sendTransaction(transaction, [walletKeyPair]);
-    res.json({ signature });
+    await connection.confirmTransaction(txid);
+    console.log(`https://solscan.io/tx/${txid}`);
 });
 
 app.use(express.json());
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
-
-
-
