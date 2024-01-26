35a883d57d04789c98358e4aa2bd65f958337a3f
Adamantios <adamantios.zaras@valory.xyz>
2024-01-26 12:13:38 +0200

    feat: introduce a flag to enable/disable the tx proxy server

--- packages/valory/skills/solana_strategy_evaluator_abci/models.py
+++ packages/valory/skills/solana_strategy_evaluator_abci/models.py
@@ -50,6 +50,12 @@ class SharedState(BaseSharedState):
         super().__init__(*args, skill_context=skill_context, **kwargs)
         self.instructions: Optional[List[Dict[str, Any]]] = None
 
+        if (
+            self.context.params.use_proxy_server
+            and self.synchronized_data.max_participants != 1
+        ):
+            raise ValueError("Cannot use proxy server with a multi-agent service!")
+
 
 def _raise_incorrect_config(key: str, values: Any) -> None:
     """Raise a `ValueError` for incorrect configuration of a nested_list workaround."""
@@ -82,6 +88,7 @@ class StrategyEvaluatorParams(BaseParams):
         self.strategies_kwargs: Dict[str, List[Any]] = nested_list_todict_workaround(
             kwargs, "strategies_kwargs"
         )
+        self.use_proxy_server: bool = self._ensure("use_proxy_server", kwargs, bool)
         super().__init__(*args, **kwargs)
 
 
