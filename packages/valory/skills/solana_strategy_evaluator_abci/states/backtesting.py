2b00655a33a602562d0ac800b2b6af55a8b858a9
Adamantios <adamantios.zaras@valory.xyz>
2024-02-13 13:06:44 +0200

    fix: add required overrides

--- packages/valory/skills/solana_strategy_evaluator_abci/states/backtesting.py
+++ packages/valory/skills/solana_strategy_evaluator_abci/states/backtesting.py
@@ -20,6 +20,7 @@
 """This module contains the backtesting state of the swap(s)."""
 
 from packages.valory.skills.abstract_round_abci.base import VotingRound, get_name
+from packages.valory.skills.solana_strategy_evaluator_abci.payloads import VotingPayload
 from packages.valory.skills.solana_strategy_evaluator_abci.states.base import (
     Event,
     SynchronizedData,
@@ -29,6 +30,8 @@ from packages.valory.skills.solana_strategy_evaluator_abci.states.base import (
 class BacktestRound(VotingRound):
     """A round in which the agents prepare swap(s) instructions."""
 
+    synchronized_data_class = SynchronizedData
+    payload_class = VotingPayload
     done_event = Event.BACKTEST_POSITIVE
     negative_event = Event.BACKTEST_NEGATIVE
     none_event = Event.BACKTEST_FAILED
