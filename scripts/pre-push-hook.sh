742cc081e19219abfaefbfb2ec56f2b31b90915f
8baller <tomrae030@hotmail.com>
2024-08-07 10:28:14 +0000

    ensured hook for pre-push

---
+++ scripts/pre-push-hook.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+set e 
+
+tox -e isort,black,pylint,mypy,flake8,safety,bandit -p
+
+# Run the tests for abci.
+tox -e check-packages 
+tox -e check-abci-docstrings
+tox -e check-handlers
+tox -e analyse-service
+tox -e check-abciapp-specs
+
+
+
+make clean
+autonomy packages lock
+git add packages
+git commit -m 'checkpoint: pre-push-hook at $(date)'
+
+echo "Pre-push hook completed successfully."
