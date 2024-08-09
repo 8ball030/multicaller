691de0b425952f758cc18107d05a622ee7ce1d48
8baller <tomrae030@hotmail.com>
2024-08-09 12:53:00 +0000

    imrpoved hook

--- scripts/pre-push-hook.sh
+++ scripts/pre-push-hook.sh
@@ -6,7 +6,7 @@ set -e
 # tox -e isort,black,pylint,mypy,flake8,safety,bandit -p 
 
 # Run the tests for abci.
-tox -e check-packages,check-abci-docstrings,check-handlers,analyse-service,check-abciapp-specs
+tox -e check-hash,check-packages,check-abci-docstrings,check-handlers,analyse-service,check-abciapp-specs
 
 
 tomte check-copyright --author valory --exclude-part abci --exclude-part http_client --exclude-part ipfs --exclude-part ledger --exclude-part p2p_libp2p_client --exclude-part gnosis_safe_proxy_factory --exclude-part multisend --exclude-part gnosis_safe --exclude-part service_registry --exclude-part protocols --exclude-part abstract_abci --exclude-part abstract_round_abci --exclude-part registration_abci --exclude-part reset_pause_abci --exclude-part solana_transaction_settlement_abci --exclude-part transaction_settlement_abci
